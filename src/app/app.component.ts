916c96e90f630aed6a264bdfe7bcc1f93463108b
taz <tazuddinleton@gmail.com>
2022-11-29 12:52:22 +0600

    Update product page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'products';
   products: any[] = [];
-  constructor() {
+  constructor(private router: Router) {
+
+
+
+  }
+  ngOnInit(): void {
+
+    // Check if user is logged in here
+    if (true) {
     fetch('https://fakestoreapi.com/products')
             .then(res=>res.json())
             .then(json=> {
               this.products = json;
               console.log(json);
             });
+    } else {
+      this.router.navigateByUrl('/login');
+    }
   }
+
+
+
 
 }
